d47d64142301895bb4217ab80a344934656d1078
Matteo Lisotto <matteo.lisotto@gmail.com>
2017-07-14 16:38:46 +0200

    Add main

---
+++ src/main/java/Main.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+import javafx.util.Pair;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main (String args[]) {
+	Map<Pair<Integer,Character>, Double> emissions = buildEmissions();
+	List<String> test = new ArrayList<>();
+	List<String> testSet = new ArrayList<>();
+	String fileName = "";
+    int[] array = new Random().ints(50, 0, 386609).toArray();
+
+	try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+                test = stream.collect(Collectors.toList());
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
+	for (int i = 0; i < array.length; i++) {
+	    int j = array[i];
+	    String s = test.get(j);
+	    testSet.add(s);
+    }
+
+
+
+	
+	HiddenMarkovModel hmm = StonModels.build("C", 4,3, emissions);
+	System.out.println(testSet);
+	System.out.println("Training");
+	hmm.train(testSet, 20);
+	hmm.print();
+    }
+
+    static Map<Pair<Integer,Character>, Double>  buildEmissions() {
+	Map<Pair<Integer,Character>, Double> em = new LinkedHashMap<>();
+	String alp = "qwertyuioplkjhgfdsazxcvbnm1234567890";
+	double prob = 1.0 / alp.length();
+
+	
+	for(int i = 0; i < 10; i++) {
+	    for(int j = 0; j < alp.length(); j++) {
+		em.put(new Pair<>(i, alp.charAt(j)), prob);
+	    }
+	}
+
+	return em;
+    }
+}
