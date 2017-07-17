1934041b00b37566ea31115315a6681717c47c6e
Matteo Lisotto <matteo.lisotto@gmail.com>
2017-07-17 19:43:25 +0200

    Add decode method in HiddenMarkovModel interface

--- src/main/java/HiddenMarkovModel.java
+++ src/main/java/HiddenMarkovModel.java
@@ -3,4 +3,5 @@ public interface HiddenMarkovModel {
     public <T> void train(T data);
     public <T> void train(T data, int steps);
     public void print();
+    public String decode(String observation);
 }
