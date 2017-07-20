599b19954bda4da5f9937fa7b856d8a9b62aff87
Matteo Lisotto <matteo.lisotto@gmail.com>
2017-07-20 11:13:39 +0200

    Add save methods in HiddenMarkovModel interface

--- src/main/java/HiddenMarkovModel.java
+++ src/main/java/HiddenMarkovModel.java
@@ -1,6 +1,10 @@
 import java.util.List;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectOutput;
+import java.io.IOException;
 
-public interface HiddenMarkovModel {
+public interface HiddenMarkovModel implements Serializable {
 
   public void train(List<String> data);
 
@@ -9,4 +13,22 @@ public interface HiddenMarkovModel {
   public void print();
 
   public String decode(String observation);
+
+  public static void save(HiddenMarkovModel hmm) {
+    try (FileOutputStream outputFile = new FileOutputStream("model.data");
+        ObjectOutputStream saveModel = new ObjectOutputStream(outputFile)) {
+      saveModel.writeObject(hmm);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void save(HiddenMarkovModel hmm, String path) {
+    try (FileOutputStream outputFile = new FileOutputStream("model.data");
+        ObjectOutputStream saveModel = new ObjectOutputStream(outputFile)) {
+      saveModel.writeObject(hmm);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
