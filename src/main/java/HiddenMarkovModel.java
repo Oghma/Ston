7cb36b90bb54d516b2b729e465f687b56bfc0682
Matteo Lisotto <matteo.lisotto@gmail.com>
2017-07-21 11:13:56 +0200

    Add load methods in HiddenMarkovModel interface

--- src/main/java/HiddenMarkovModel.java
+++ src/main/java/HiddenMarkovModel.java
@@ -1,10 +1,12 @@
 import java.util.List;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
-import java.io.ObjectOutput;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.io.FileInputStream;
 
-public interface HiddenMarkovModel implements Serializable {
+public interface HiddenMarkovModel extends Serializable {
 
   public void train(List<String> data);
 
@@ -30,5 +32,33 @@ public interface HiddenMarkovModel implements Serializable {
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  public static HiddenMarkovModel load() {
+    HiddenMarkovModel hmm = null;
+    try (FileInputStream inputFile = new FileInputStream("model.data");
+        ObjectInputStream loadModel = new ObjectInputStream(inputFile)) {
+      hmm = (HiddenMarkovModel) loadModel.readObject();
+    } catch (IOException i) {
+      i.printStackTrace();
+    } catch (ClassNotFoundException c) {
+      System.out.println("HiddenMarkovModel class not found");
+      c.printStackTrace();
+    }
+    return hmm;
+  }
+
+  public static HiddenMarkovModel load(String path) {
+    HiddenMarkovModel hmm = null;
+    try (FileInputStream inputFile = new FileInputStream(path);
+        ObjectInputStream loadModel = new ObjectInputStream(inputFile)) {
+      hmm = (HiddenMarkovModel) loadModel.readObject();
+    } catch (IOException i) {
+      i.printStackTrace();
+    } catch (ClassNotFoundException c) {
+      System.out.println("HiddenMarkovModel class not found");
+      c.printStackTrace();
+    }
+    return hmm;
   }
 }
