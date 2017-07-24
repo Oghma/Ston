7886b40f425777e12eedf3f323c51e019e70c5c4
Matteo Lisotto <matteo.lisotto@gmail.com>
2017-07-24 14:40:48 +0200

    Rewrite build method in StonModels class

--- src/main/java/StonModels.java
+++ src/main/java/StonModels.java
@@ -3,31 +3,21 @@ import java.util.Map;
 import javafx.util.Pair;
 
 public class StonModels {
-    public static HiddenMarkovModel build(String topology, int numPrefixes, int numSuffixes, List<Character> sigma) {
-	HiddenMarkovModel hmm = null;
-	switch(topology) {
-	case "A":
-	    break;
-	case "B":
-	    break;
-	case "C":
-	    hmm = new HiddenMarkovModelC(numPrefixes, numSuffixes, sigma);
-	    break;
-	}
-	return hmm;
+  public static HiddenMarkovModel build(
+      String topology, boolean smoothed, int numPrefixes, int numSuffixes, List<Character> sigma) {
+    HiddenMarkovModel hmm = null;
+    switch (topology) {
+      case "A":
+        break;
+      case "B":
+        break;
+      case "C":
+        hmm =
+            smoothed == true
+                ? new HiddenMarkovModelCSmoothed(numPrefixes, numSuffixes, sigma)
+                : new HiddenMarkovModelC(numPrefixes, numSuffixes, sigma);
+        break;
     }
-
-    public static HiddenMarkovModel build(String topology, int numPrefixes, int numSuffixes, List<Character> sigma, Map<Pair<Integer, Character>, Double> emissions) {
-	HiddenMarkovModel hmm = null;
-	switch(topology) {
-	case "A":
-	    break;
-	case "B":
-	    break;
-	case "C":
-	    hmm = new HiddenMarkovModelC(numPrefixes, numSuffixes, sigma, emissions);
-	    break;
-	}
-	return hmm;
-    }
+    return hmm;
+  }
 }
